157cdba38cc5e0e5ddd9b2e0f93a02668252d6e3
SIST <SIST@DESKTOP-V68OO4H>
2026-07-28 10:25:07 +0900

    UserController 생성

---
+++ LibrarySystem/src/user_controller/UserController.java
@@ -0,0 +1,17 @@
+package user_controller;
+
+import java.util.Scanner;
+
+public class UserController {
+
+	
+	// *** 시작메뉴를 보여주는 메서드 ***
+	public void mainstart(Scanner sc) {
+		
+
+		
+	} // end of public void mainstart(Scanner sc)----------
+	
+	
+
+}
