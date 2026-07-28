d2ef84eb5cb3bd9c4452663e11a10ca55b7cc86e
SIST <SIST@DESKTOP-V68OO4H>
2026-07-28 10:25:21 +0900

    Main 클래스 생성

--- LibrarySystem/src/main/Main.java
+++ LibrarySystem/src/main/Main.java
@@ -1,10 +1,27 @@
 package main;
 
+import java.util.Scanner;
+
+import dbconnection.ProjectDBConnection;
+import user_controller.UserController;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+		
+		UserController userCtrl = new UserController();
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		userCtrl.mainstart(sc);
+		
+		
+		
+		ProjectDBConnection.closeConnection();
+		System.out.println("~~~~ 프로그램 종료 ~~~~");
+		
+	} // end of public static void main(String[] args)---------
 
 }
