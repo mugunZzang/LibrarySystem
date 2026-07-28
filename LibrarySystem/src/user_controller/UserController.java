21533d6120503e4e7aa3a814dd9b10a42e566918
SIST <SIST@DESKTOP-V68OO4H>
2026-07-28 10:33:56 +0900

    UserController 시작 메뉴 틀 제작

--- LibrarySystem/src/user_controller/UserController.java
+++ LibrarySystem/src/user_controller/UserController.java
@@ -8,7 +8,32 @@ public class UserController {
 	// *** 시작메뉴를 보여주는 메서드 ***
 	public void mainstart(Scanner sc) {
 		
+		System.out.println("\n--------------- 로그인전 ---------------\n"
+		         + "1.    2.    3. 로그인   4. 회원가입   \n");
 
+		System.out.print("▷ 메뉴번호 선택 : ");
+		String menuNo = sc.nextLine();
+		
+		switch (menuNo) {
+		
+			case "1":
+				
+				break;
+			case "2":
+				
+				break;
+			case "3":
+				
+				break;
+			case "4":
+				
+				break;
+
+			default:
+				break;
+		}
+		
+		
 		
 	} // end of public void mainstart(Scanner sc)----------
 	
